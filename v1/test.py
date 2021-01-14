04633e522ba47411f6032e0aa53676f8062243b0
YoannSo <58392344+YoannSo@users.noreply.github.com>
2021-01-14 11:06:22 +0100

    commit

--- v1/test.py
+++ v1/test.py
@@ -7,7 +7,8 @@ afn = 	[ [0,"a",0],
 		[2,"b",1], 
 		[3,"a",3], 
 		[3,"b",1] ]
-
+listEtat=[0,1,2,3]
+listTransition=["a","b"]
 # for i in range (len(afn)):
 
 # 	currentState = afn[i][0]
@@ -41,13 +42,28 @@ def findPath(afn, list, char):
 	#print("from", list, "->", res)
 	return res
 
-
-
+listA=[[0]]
+listB=[[0]]
+def afn2afd(afn):
+	afd=[]
+	for i in range(0,3):
+		resA=findPath(afn,listA[i],"a")
+		resB=findPath(afn,listB[i],"b")
+		listA.append(resA)
+		listB.append(resB)
+	for i in range(1,len(listA)):
+		afd.append(findPath(afn,listA[i],"b"))
+		print(afd)
+	for i in range(1,len(listB)):
+		afd.append(findPath(afn,listB[i],"a"))
+		print(afd)
+	print(listA)
+	print(listB)
 #do state 0 with a, b 
 #do state ... (that was created from previous request) with a, b
 #etc....
 #if the answer from the function was already treated no longer need to do it again
-
+afn2afd(afn)
 #findPath(afn, [0], "a")
 # findPath(afn, [1], "a")
 # findPath(afn, [2], "a")
@@ -71,8 +87,8 @@ def findPath(afn, list, char):
 # findPath(afn, [0,2,3], "b")
 
 #MANUAL TEST
-findPath(afn, [0], "a")
-findPath(afn, [0], "b")
+#findPath(afn, [0], "a")
+#findPath(afn, [0], "b")
 
 # findPath(afn, [0,2], "a")
 # findPath(afn, [0,2], "b")
